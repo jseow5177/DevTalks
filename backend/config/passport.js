7097762e3080e001283e77ead070e7c34f99a470
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-19 16:54:06 +0800

    Add passport strategy

---
+++ backend/config/passport.js
@@ -0,0 +1,29 @@
+// Setup passport-jwt authentication strategy
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
+const keys = require('./keys');
+
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // Extract token from auth header
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+    // Set up passport strategy
+    passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+        try {
+            // Take user id from jwt_payload
+            const foundUser = User.findById(jwt_payload.id);
+            if (foundUser) {
+                return done(null, foundUser);
+            } else {
+                return done(null, false);
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    })
+)};
