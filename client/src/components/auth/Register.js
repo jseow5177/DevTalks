6f2752226b436ab8293174b5c5123b2631407e76
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-24 13:31:57 +0800

    Add register form component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
 import FormRow from './FormRow';
+import { registerUser } from '../../actions/authActions';
 
 import EmailIcon from '@material-ui/icons/Email';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import FaceIcon from '@material-ui/icons/Face';
 import LockIcon from '@material-ui/icons/Lock';
 
-function Register({ show, handleCloseRegister }) {
+function Register({ show, handleCloseRegister, errors, registerUser }) {
 
     const [registerForm, setRegisterForm] = useState({
         email: "",
@@ -19,9 +21,18 @@ function Register({ show, handleCloseRegister }) {
         password: "",
         confirmPassword: ""
     });
+    const [formErrors, setFormErrors] = useState({});
 
+    useEffect(() => {
+        setFormErrors(errors);
+    }, [errors]);
+ 
     const handleChange = (event) => {
         setRegisterForm({ ...registerForm, [event.target.id]: event.target.value });
+    }
+
+    const handleFormSubmit = () => {
+        registerUser(registerForm);
     }
 
     return (
@@ -30,7 +41,7 @@ function Register({ show, handleCloseRegister }) {
                 <Modal.Header closeButton>
                     <Modal.Title>Join our community!</Modal.Title>
                 </Modal.Header>
-                <Form className="landing-form">
+                <Form className="landing-form" onSubmit={handleFormSubmit}>
                     <FormRow
                         id="email"
                         placeholder="Email"
@@ -38,6 +49,7 @@ function Register({ show, handleCloseRegister }) {
                         icon={<EmailIcon />}
                         value={registerForm.email}
                         handleChange={handleChange}
+                        error={formErrors.email}
                     />
                     <FormRow
                         id="username"
@@ -46,30 +58,43 @@ function Register({ show, handleCloseRegister }) {
                         icon={<FaceIcon />}
                         value={registerForm.username}
                         handleChange={handleChange}
+                        error={formErrors.username}
                     />
                     <FormRow
                         id="password"
-                        placeholder="Password" 
-                        type="password" 
-                        icon={<VpnKeyIcon />} 
-                        value={registerForm.password} 
-                        handleChange={handleChange} 
+                        placeholder="Password"
+                        type="password"
+                        icon={<VpnKeyIcon />}
+                        value={registerForm.password}
+                        handleChange={handleChange}
+                        error={formErrors.password}
                     />
-                    <FormRow 
-                        id="confirmPassword" 
-                        placeholder="Confirm password" 
-                        type="password" 
-                        icon={<LockIcon />} 
-                        value={registerForm.confirmPassword} 
-                        handleChange={handleChange} 
+                    <FormRow
+                        id="confirmPassword"
+                        placeholder="Confirm password"
+                        type="password"
+                        icon={<LockIcon />}
+                        value={registerForm.confirmPassword}
+                        handleChange={handleChange}
+                        error={formErrors.confirmPassword}
                     />
                 </Form>
                 <Modal.Footer>
-                    <Button variant="success">Register</Button>
+                    <Button variant="success" onClick={handleFormSubmit}>Register</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
 }
 
-export default Register;
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        registerUser: (registerForm, history) => dispatch(registerUser(registerForm, history))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Register));
