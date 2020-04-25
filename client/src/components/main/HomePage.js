2338dd3fb0552c037f6dad57b48d4041d875cb13
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-25 15:50:13 +0800

    Refactor home page

--- client/src/components/main/HomePage.js
+++ client/src/components/main/HomePage.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import { startSocket } from '../../actions/socketActions';
 
@@ -8,8 +9,11 @@ import SideBar from './SideBar';
 import Header from './Header';
 import Body from './Body';
 
-function HomePage({ startSocket }) {
+function HomePage({ startSocket, auth }) {
 
+    const [joinedChannels, setJoinedChannels] = useState([]);
+    const [starredChannels, setStarredChannels] = useState([]);
+    const [someoneJoinedChannel, setSomeoneJoinedChannel] = useState("");
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -21,19 +25,40 @@ function HomePage({ startSocket }) {
 
         return () => {
             socket.emit('disconnect');
-            socket.off();
         }
 
     }, [startSocket]);
 
+    useEffect(() => {
+
+        axios.get(`http://localhost:5000/dev-talks/users/${auth.user.id}/channels/`).then(res => {
+            const userChannels = res.data;
+            setJoinedChannels(userChannels.joinedChannels);
+            setStarredChannels(userChannels.starredChannels);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }, [auth.user.id, someoneJoinedChannel]);
+
     return (
         <div className="main">
             <Header />
-            <SideBar />
-            <Body />
+            <SideBar 
+                joinedChannels={joinedChannels} 
+                starredChannels={starredChannels}
+                setJoinedChannels={setJoinedChannels}
+                setStarredChannels={setStarredChannels} />
+            <Body
+                someoneJoinedChannel={someoneJoinedChannel} 
+                setSomeoneJoinedChannel={setSomeoneJoinedChannel} />
         </div>
     )
 }
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
 
 const mapDispatchToProps = (dispatch) => {
     return {
@@ -41,4 +66,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(HomePage);
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
