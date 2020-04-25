86879c06f22d7b3aebb9a4709ac2ae56fbaf3c0f
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-25 23:50:35 +0800

    Add redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+
+import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { removeActiveChannel } from './actions/channelActions';
 
 /* Styles */
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +14,28 @@ import './App.css';
 
 import Landing from './components/auth/Landing';
 import HomePage from './components/main/HomePage';
+import PrivateRoute from './components/PrivateRoute';
+
+// Check for token in the localStorage of browser
+// If token exists, user is automatically authenticated
+// If token expires, logout user
+if (localStorage.jwtToken) {
+  // Get the token
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and set current user
+  const decoded = jwt_decode(token);
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check if token is expired
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    console.log('logout!');
+    store.dispatch(logoutUser());
+    store.dispatch(removeActiveChannel());
+  }
+
+}
 
 function App() {
   return (
@@ -17,7 +44,7 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/" component={Landing} />
-            <Route exact path="/dev-talks" component={HomePage} />
+            <PrivateRoute exact path="/dev-talks" component={HomePage} />
           </Switch>
         </Router>
       </div>
