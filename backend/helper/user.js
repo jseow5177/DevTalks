123878cac5957ae5409fdb2b3c32b10c28e99cf6
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-26 15:12:32 +0800

    Add helper functions to handle user login and registration

--- backend/helper/user.js
+++ backend/helper/user.js
@@ -12,7 +12,7 @@ const User = require('../models/User');
 
 const registerUser = async (req, res) => {
 
-    const {errors, isValid} = validateRegisterInput(req.body);
+    const { errors, isValid } = validateRegisterInput(req.body);
 
     if (!isValid) {
         return res.status(400).json(errors);
@@ -20,10 +20,10 @@ const registerUser = async (req, res) => {
 
     // Check if email exists
     try {
-        const foundUserWithSameEmail = await User.findOne({email: req.body.email});
+        const foundUserWithSameEmail = await User.findOne({ email: req.body.email });
 
         if (foundUserWithSameEmail) {
-            return res.status(400).json({email: 'Email already exists'});
+            return res.status(400).json({ email: 'Email already exists' });
         } else {
 
             const newUser = new User({
@@ -36,23 +36,24 @@ const registerUser = async (req, res) => {
 
             bcrypt.genSalt(10, async (err, salt) => {
                 if (err) {
-                    return res.status(500).json({message: 'genSalt error'});
+                    return res.status(500).json({ message: 'genSalt error' });
                 }
                 const hash = await bcrypt.hash(newUser.password, salt);
                 newUser.password = hash;
                 await newUser.save();
-                return res.status(200).json({message: 'New user created'});
+                // Upon successful sign up, user immediately log ins
+                return res.status(200).json({ email: req.body.email, password: req.body.password });
             });
         }
-    } catch(err) {
-        return res.status(500).json({message: error.message});
+    } catch (err) {
+        return res.status(500).json({ message: error.message });
     }
 
 }
 
 const loginUser = async (req, res) => {
 
-    const {errors, isValid} = validateLoginInput(req.body);
+    const { errors, isValid } = validateLoginInput(req.body);
 
     if (!isValid) {
         return res.status(400).json(errors);
@@ -62,10 +63,10 @@ const loginUser = async (req, res) => {
     const password = req.body.password;
 
     try {
-        const foundUser = await User.findOne({email: email});
+        const foundUser = await User.findOne({ email: email });
         // Check if user exists
         if (!foundUser) {
-            return res.status(404).json({emailNotFound: 'Email not found'});
+            return res.status(404).json({ emailNotFound: 'Email not found' });
         } else {
             // Compare password
             const isMatch = await bcrypt.compare(password, foundUser.password);
@@ -76,17 +77,31 @@ const loginUser = async (req, res) => {
                     username: foundUser.username
                 }
                 // Sign token with secret key
-                jwt.sign(jwt_payload, keys.secretOrKey, {expiresIn: 31556926}, (err, token) => {
-                    return res.status(200).json({success: true, token: `Bearer ${token}`})
+                jwt.sign(jwt_payload, keys.secretOrKey, { expiresIn: '30d' }, (err, token) => {
+                    return res.status(200).json({ success: true, token: `Bearer ${token}` })
                 });
             } else {
-                return res.status(400).json({passwordIncorrect: 'Password incorrect'});
+                return res.status(400).json({ passwordIncorrect: 'Password incorrect' });
             }
         }
-    } catch(err) {
-        return res.status(500).json({message: err.message});
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
 
 }
 
-module.exports = {registerUser, loginUser}
+const findUser = async (userId) => {
+
+    try {
+        const foundUser = await User.findOne({ _id: userId });
+        if (!foundUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return foundUser;
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+}
+
+module.exports = { registerUser, loginUser, findUser }
