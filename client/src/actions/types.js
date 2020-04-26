da86c937d3a35363df5848f03bb21db5e95c9e19
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-26 15:15:31 +0800

    Add notification action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,17 @@
+// Handle socket instance of user
 export const USER_ONLINE = 'USER_ONLINE';
+
+// Handle channel under view by user
 export const ACTIVE_CHANNEL = 'ACTIVE_CHANNEL';
+export const NO_ACTIVE_CHANNEL = 'NO_ACTIVE_CHANNEL';
+
+// Handle authentication
+export const GET_ERRORS = 'GET_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+// export const USER_LOADING = 'USER_LOADING';
+// export const USER_JOIN = 'USER_JOIN';
+
+// Handle notifications
+export const HAS_NOTIFICATIONS = 'HAS_NOTIFICATIONS';
+export const REMOVE_NOTIFICATIONS = 'REMOVE_NOTIFICATIONS';
