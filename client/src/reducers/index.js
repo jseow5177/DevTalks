df264ce0afe26a6ac79dba540495a84c8bf746ed
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-26 21:59:58 +0800

    Add root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import socketReducer from './socketReducer';
 import channelReducer from './channelReducer';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import notificationReducer from './notificationReducer';
 
 export default combineReducers({
     socket: socketReducer,
     activeChannel: channelReducer,
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    notifications: notificationReducer
 });
