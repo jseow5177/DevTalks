49ce8ca9c4db62d8dbc3bb522d31e6083e4cfe9c
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-04-30 19:09:31 +0800

    Add removeActiveChannel action creator

--- client/src/actions/channelActions.js
+++ client/src/actions/channelActions.js
@@ -1,7 +1,7 @@
-import { ACTIVE_CHANNEL } from "./types";
+import { ACTIVE_CHANNEL, NO_ACTIVE_CHANNEL } from "./types";
 import axios from 'axios';
 
-// Initialise user socket
+// Get the channel currently viewed by user
 export const getActiveChannel = (channelId) => {
     return (dispatch) => {
         axios.get(`http://localhost:5000/dev-talks/channels/${channelId}`).then(res => {
@@ -9,8 +9,16 @@ export const getActiveChannel = (channelId) => {
                 type: ACTIVE_CHANNEL,
                 payload: res.data
             });
-        }).catch(err => {
-            console.log(err.response.data);
+        }).catch(err => console.log(err));
+    };
+};
+
+export const removeActiveChannel = () => {
+    return (dispatch) => {
+        dispatch({
+            type: NO_ACTIVE_CHANNEL,
+            payload: null
         });
-    }
-}
+    };
+};
+
