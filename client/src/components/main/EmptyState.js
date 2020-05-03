bf1fa5822ee1e55440cddf27955d230ba9cab8ca
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-03 16:07:28 +0800

    Add chat body dummy page

--- client/src/components/main/EmptyState.js
+++ client/src/components/main/EmptyState.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { sendNotifications } from '../../actions/notificationActions';
 
 import Image from 'react-bootstrap/Image';
 
-function EmptyState() {
+function EmptyState({ socketInstance, sendNotifications, auth }) {
+
+    // Allows user to receive notifications when they are still in EmptyState
+    useEffect(() => {
+        if (socketInstance.socket) {
+            socketInstance.socket.on('message', messageData => {
+                const senderId = messageData.message.from.userId;
+                if (senderId !== auth.user.id) {
+                    sendNotifications(messageData.channelId);
+                }
+            });
+            return () => {
+                socketInstance.socket.off('message');
+            }
+        }
+
+    }, [socketInstance.socket, sendNotifications, auth]);
+
     return (
         <div className="empty-state-wrapper">
             <Image src={`${process.env.PUBLIC_URL}/empty-state.png`} roundedCircle />
@@ -12,4 +32,15 @@ function EmptyState() {
     )
 }
 
-export default EmptyState;
+const mapStateToProps = state => ({
+    auth: state.auth,
+    socketInstance: state.socket
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        sendNotifications: (channelId) => dispatch(sendNotifications(channelId))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmptyState);
