682325af7f127adb938df977bc20abe351fcfc6c
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-10 14:51:20 +0800

    Add starChannel, getUnreadChannelMessages and readChannelMessages

--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const { 
     getAllUserChannels, 
-    getChannelInfo, 
     addNewChannel, 
     addNewMessage, 
     getAllChannels, 
     joinChannel,
-    leaveChannel 
+    leaveChannel,
+    starChannel,
+    getUnreadChannelMessages,
+    getChannelInfo,
+    readChannelMessages
 } = require('../helper/channel.js');
 
 /* Get channels of a user and display them on the side bar */
@@ -20,7 +23,6 @@ router.route('/users/:userId/channels').get(async (req, res) => {
 
 /* Get a specific channel clicked by user on the side bar and display all infomation of the channel */
 router.route('/channels/:channelId').get(async (req, res) => {
-
     const foundChannel = await getChannelInfo(req, res);
     if (!foundChannel) {
         return res.status(404).json({ message: 'Channel not found' });
@@ -44,21 +46,41 @@ router.route('/channels/:channelId/messages/new-message').post(async (req, res) 
 });
 
 /* Get all available channels */
-router.route('/channels/').get(async (req, res) => {
+router.route('/channels').get(async (req, res) => {
 
     await getAllChannels(req, res);
     
 });
 
-router.route('/channels/:channelId/users/add').put(async (req, res) => {
+/* When user joins a channel */
+router.route('/channels/:channelId/join').put(async (req, res) => {
 
     await joinChannel(req, res);
 
 });
 
+/* When user leaves a channel */
 router.route('/channels/:channelId/users/:userId/leave').delete(async (req, res) => {
 
     await leaveChannel(req, res);
+
+});
+
+router.route('/channels/:channelId/users/:userId/star').put(async (req, res) => {
+
+    await starChannel(req, res);
+
+});
+
+router.route('/channels/:channelId/read-by/:userId').get(async (req, res) => {
+
+    await getUnreadChannelMessages(req, res);
+
+});
+
+router.route('/channels/:channelId/read-by/:userId').post(async (req, res) => {
+
+    await readChannelMessages(req, res);
 
 });
 
