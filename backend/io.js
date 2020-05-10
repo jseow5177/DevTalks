d7b817bbc4510a0fe6ebcad012f2b955e1fd29d4
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-10 14:40:54 +0800

    Add socket listeners to deal with notifications

--- backend/io.js
+++ backend/io.js
@@ -1,25 +1,34 @@
 module.exports = (io) => {
 
-    io.on('connection', (socket) => {
+    io.on('connection', socket => {
         console.log('I am connected!');
         
         // When app is loaded, add user's socket into all his or her channels
-        socket.on('joinChannel', (joinedChannels) => {
+        socket.on('joinChannel', joinedChannels => {
             joinedChannels.forEach(joinedChannel => {
                 socket.join(joinedChannel.channelId);
             });
         });
 
         // When app is loaded, add user's socket into all his or her conversations
-        socket.on('joinConversation', (joinedConversations) => {
+        socket.on('joinConversation', joinedConversations => {
             joinedConversations.forEach(joinedConversation => {
                 socket.join(joinedConversation);
             });
         });
+
+        socket.on('joinNewConversation', newConversation => {
+            socket.join(newConversation);
+        });
     
-        // Receive new message and emit to all clients in the channel
-        socket.on('sendMessage', (data) => {
+        // Receive new message and emit to all clients in the channel or conversation
+        socket.on('sendMessage', data => {
             io.to(data.id).emit('message', data);
+            
+        });
+
+        socket.on('notification', data => {
+            socket.broadcast.to(data.id).emit('notification', data);
         });
     
         // When a user joins a channel:
@@ -27,17 +36,17 @@ module.exports = (io) => {
         // 2. Perform live update on the body (eg: Update SideBar, update number of participants on ChannelInfo)
         // 3. Emit an admin message that says a new user has joined
         socket.on('newMember', data => {
-            socket.join(data.channelId);
-            io.to(data.channelId).emit('newMember', data.channelId);
-            io.to(data.channelId).emit('message', data);
+            socket.join(data.id);
+            io.to(data.id).emit('newMember', data.id);
+            io.to(data.id).emit('message', data);
         });
 
         // When a user joins a channel:
         // 1. Perform live update on the body (eg: Update SideBar, update number of participants on ChannelInfo)
         // 2. Emit an admin message that says a new user has joined
         socket.on('userLeft', data => {
-            io.to(data.channelId).emit('userLeft', data.channelId);
-            io.to(data.channelId).emit('message', data);
+            io.to(data.id).emit('userLeft', data.id);
+            io.to(data.id).emit('message', data);
         });
         
         // Show <username> is typing...
@@ -49,8 +58,9 @@ module.exports = (io) => {
             io.emit('newFriendRequest', friendRequestData);
         });
 
-        socket.on('acceptFriendRequest', friendRequestData => {
-            io.emit('acceptFriendRequest', friendRequestData);
+        socket.on('acceptFriendRequest', data => {
+            io.emit('acceptFriendRequest', data.friendRequestData);
+            io.emit('joinNewConversation', data);
         });
 
         socket.on('cancelFriendRequest', friendRequestData => {
