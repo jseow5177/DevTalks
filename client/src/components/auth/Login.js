125ca0025b5019bb8df919b8c04fc9c1aabb6893
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-12 23:49:16 +0800

    Remove Row and Col

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,10 +4,8 @@ import { useHistory } from 'react-router-dom';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
-import FormRow from './FormRow';
+import FormRow from '../main/FormRow';
 import { loginUser } from '../../actions/authActions';
 
 import CodeIcon from '@material-ui/icons/Code';
@@ -29,56 +27,52 @@ function Login({ handleShowRegister, errors, loginUser, auth }) {
         setFormErrors(errors);
     }, [errors]);
 
-    // Check if user is authenticated
+    // Check if user is authenticated. If authenticated, move to HomePage
     useEffect(() => {
-
         if (auth.isAuthenticated) {
             history.push('/dev-talks');
         }
-           
     }, [auth, history]);
 
+    // Handle input change in form
     const handleChange = (event) => {
         setLoginForm({ ...loginForm, [event.target.id]: event.target.value });
     }
 
+    // Handle form submit
     const handleFormSubmit = () => {
         loginUser(loginForm);
     }
 
     return (
         <div>
-            <Row>
-                <Col xs={12}>
-                    <div className="landing-title">
-                        <h2 className="app-name"><CodeIcon className="logo" fontSize="large" />Dev Talks</h2>
-                    </div>
-                    <div className="landing-subtitle">
-                        <h6>A chat space for developers</h6>
-                    </div>
-                    <Form className="landing-form">
-                        <FormRow
-                            id="email"
-                            placeholder="Email"
-                            type="email"
-                            icon={<EmailIcon />}
-                            value={loginForm.email}
-                            handleChange={handleChange}
-                            error={formErrors.emailNotFound || formErrors.email}
-                        />
-                        <FormRow
-                            id="password"
-                            placeholder="Password"
-                            type="password"
-                            icon={<VpnKeyIcon />}
-                            value={loginForm.password}
-                            handleChange={handleChange}
-                            error={formErrors.passwordIncorrect || formErrors.password}
-                        />
-                        <Button variant="success" onClick={handleFormSubmit}>Login</Button>
-                    </Form>
-                </Col>
-            </Row>
+            <div className="landing-title">
+                <h2 className="app-name"><CodeIcon className="logo" fontSize="large" />Dev Talks</h2>
+            </div>
+            <div className="landing-subtitle">
+                <h6>A chat space for developers</h6>
+            </div>
+            <Form className="landing-form">
+                <FormRow
+                    id="email"
+                    placeholder="Email"
+                    type="email"
+                    icon={<EmailIcon />}
+                    value={loginForm.email}
+                    handleChange={handleChange}
+                    error={formErrors.emailNotFound || formErrors.email}
+                />
+                <FormRow
+                    id="password"
+                    placeholder="Password"
+                    type="password"
+                    icon={<VpnKeyIcon />}
+                    value={loginForm.password}
+                    handleChange={handleChange}
+                    error={formErrors.passwordIncorrect || formErrors.password}
+                />
+                <Button variant="success" onClick={handleFormSubmit}>Login</Button>
+            </Form>
             <p className="register-or-login-text">Don't have an account? <button onClick={handleShowRegister} className="register-or-login-link">Register here</button>.</p>
         </div>
 
