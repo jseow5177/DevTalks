a33f4845143d92acfd1f04dae56bd15ee248ae03
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-12 23:48:23 +0800

    Edit in code styling

--- client/src/components/auth/Landing.js
+++ client/src/components/auth/Landing.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Login from './Login';
 import Register from './Register';
@@ -6,13 +6,13 @@ import Register from './Register';
 function Landing() {
 
     const [show, setShow] = useState(false);
-  
+
     const handleCloseRegister = () => setShow(false);
     const handleShowRegister = () => setShow(true);
 
     return (
         <div className='landing-wrapper'>
-            <Login handleShowRegister={handleShowRegister}/>
+            <Login handleShowRegister={handleShowRegister} />
             <Register show={show} handleCloseRegister={handleCloseRegister} />
         </div>
     )
