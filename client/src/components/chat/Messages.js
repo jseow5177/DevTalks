0ab59bbbf44768a2937b687bd0690cef0e63db91
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-17 14:05:40 +0800

    Add date logic

--- client/src/components/chat/Messages.js
+++ client/src/components/chat/Messages.js
@@ -1,32 +1,56 @@
 import React, { useState, useEffect } from 'react';
 
 import Message from './Message';
+import { getDate } from '../../utils/date';
 
-function Messages({ messages }) {
+function Messages({ messages, messageRef }) {
 
     const [messagesByDate, setMessagesByDate] = useState([]);
-    const [date, setDate] = useState("");
+    const [date, setDate] = useState('');
 
     useEffect(() => {
+
         setMessagesByDate(messages.messages);
-        setDate(messages.date);
+
+        let date = messages.date;
+
+        const today = new Date();
+        const yesterday = new Date(today);
+
+        yesterday.setDate(yesterday.getDate() - 1);
+
+        const dateToday = getDate(today);
+        const dateYesterday = getDate(yesterday);
+
+        if (date === dateToday) {
+            setDate('Today');
+        } else if (date === dateYesterday) {
+            setDate('Yesterday');
+        } else {
+            setDate(messages.date);
+        }
+
     }, [messages]);
 
     return (
         <div>
-            <div className="date-wrapper">
-                <div className="date">{date}</div>
+            <div className='date-wrapper'>
+                <div className='date'>{date}</div>
             </div>
-            <div>
-                {messagesByDate.map((message, index) =>
+            {
+                messagesByDate.map(message => (
                     <Message
-                        key={index}
+                        key={message._id}
+                        id={message._id}
+                        messageType={message.messageType}
                         username={message.from.username}
+                        userId={message.from.userId}
                         message={message.message}
                         time={message.time}
-                    />)
-                }
-            </div>
+                        messageRef={messageRef}
+                    />
+                ))
+            }
         </div>
     )
 }
