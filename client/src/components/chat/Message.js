ea68600d96c153dda4f914b88f7d7eb2b34fd885
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-17 14:01:19 +0800

    Add logic to extract message date and time

--- client/src/components/chat/Message.js
+++ client/src/components/chat/Message.js
@@ -2,35 +2,75 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import ReactEmoji from 'react-emoji';
 
-function Message({ username, message, time, auth }) {
+import { getTime } from '../../utils/date';
+import { getActiveProfile } from '../../actions/profileActions';
+import { removeActiveChannel } from '../../actions/channelActions';
+
+function Message({ username, userId, message, time, auth, messageType, getActiveProfile, removeActiveChannel, messageRef, id, notification }) {
+
+    const [unreadMessages, setUnreadMessages] = useState([]);
+
+    useEffect(() => {
+        setUnreadMessages(notification.unreadMessages);
+    }, [notification.unreadMessages]);
 
     const [messageTime, setMessageTime] = useState(null);
 
     // Process message time stamp to extract the time and render them
     useEffect(() => {
-
-        const dateObject = new Date(time);
-        const hour = (dateObject.getHours() < 10 ? '0' : '') + dateObject.getHours();
-        const minute = (dateObject.getMinutes() < 10 ? '0' : '') + dateObject.getMinutes();
-        setMessageTime(`${hour}:${minute}`);
-
+        setMessageTime(getTime(time));
     }, [time]);
 
+    const getUserInfo = (event) => {
+        const userId = event.target.id;
+        // Show user profile
+        getActiveProfile(userId);
+        removeActiveChannel();
+    }
+
     return (
-        <div>
-            <div className={'message-container ' + (auth.user.username === username ? 'message-justify-end' : null)}>
-                <div className={'message-box ' + (auth.user.username === username ? 'message-box-purple' : 'message-box-grey')}>
-                    {auth.user.username === username ? null : <h6>{username}</h6>}
-                    <p>{ReactEmoji.emojify(message)}</p>
-                    <p className="time">{messageTime}</p>
+        <div ref={unreadMessages && id === unreadMessages[0] ? messageRef : null}>
+            {unreadMessages && id === unreadMessages[0] ?
+                <div className='unread-message-container'>
+                    <div className='unread-message'>{unreadMessages.length} UNREAD {unreadMessages.length > 1 ? 'MESSAGES' : 'MESSAGE'}</div>
                 </div>
-            </div>
+                : null
+            }
+            {
+                <div>
+                    {
+                        messageType !== 'admin'
+                            ? <div className={'message-container ' + (auth.user.username === username ? 'message-justify-end' : null)}>
+                                <div className={'message-box ' + (auth.user.username === username ? 'message-box-purple' : 'message-box-grey')}>
+                                    {auth.user.username === username ? null : <h6 id={userId} className='username-link' onClick={getUserInfo}>{username}</h6>}
+                                    <p>{ReactEmoji.emojify(message)}</p>
+                                    <p className="time">{messageTime}</p>
+                                </div>
+                            </div>
+                            : <div className='admin-message-box'>
+                                {
+                                    username === auth.user.username
+                                        ? <p>You have {message}</p>
+                                        : <p><span id={userId} className='username-link' onClick={getUserInfo}>{username}</span> has {message}</p>
+                                }
+                            </div>
+                    }
+                </div>
+            }
         </div>
     )
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
-})
+    auth: state.auth,
+    notification: state.notification
+});
 
-export default connect(mapStateToProps, null)(Message);
+const mapDispatchToProps = dispatch => {
+    return {
+        getActiveProfile: (userId) => dispatch(getActiveProfile(userId)),
+        removeActiveChannel: () => dispatch(removeActiveChannel())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Message);
