58e6f9abce5f4b2dd151de668ce9e552f8510fff
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-17 14:07:59 +0800

    Add reducer for FIRST_UNREAD

--- client/src/reducers/notificationReducer.js
+++ client/src/reducers/notificationReducer.js
@@ -1,24 +1,13 @@
-import { HAS_NOTIFICATIONS, REMOVE_NOTIFICATIONS } from "../actions/types";
+import { FIRST_UNREAD } from "../actions/types";
 
 const initialState = {}
 
 export default function (state = initialState, action) {
     switch(action.type) {
-        case HAS_NOTIFICATIONS:
-            if (!state[action.payload]) {
-                return {
-                    ...state,
-                    [action.payload]: 1
-                };
-            } else {
-                return {
-                    ...state,
-                    [action.payload]: state[action.payload] + 1
-                };
+        case FIRST_UNREAD:
+            return {
+                unreadMessages: action.payload
             }
-        case REMOVE_NOTIFICATIONS:
-            delete state[action.payload];
-            return state;
         default: {
             return state;
         }
