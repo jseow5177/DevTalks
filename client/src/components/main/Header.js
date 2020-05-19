e9ecf98de793c9555ab7b72ac0fa8574e3c392b9
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-05-19 23:36:09 +0800

    Remove data request logic

--- client/src/components/main/Header.js
+++ client/src/components/main/Header.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import _ from 'lodash';
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -11,17 +10,18 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 import ChannelList from './ChannelList';
-import FormRow from '../auth/FormRow';
+import FormRow from '../main/FormRow';
 import SearchResult from './SearchResult';
 import { logoutUser } from '../../actions/authActions';
 import { removeActiveChannel } from '../../actions/channelActions';
+import { removeActiveProfile } from '../../actions/profileActions';
 
-function Header({ logoutUser, removeActiveChannel }) {
+function Header({ logoutUser, removeActiveChannel, removeActiveProfile, allChannels }) {
 
     const [query, setQuery] = useState('');
     const [allChannelNames, setAllChannelNames] = useState([]); // Hold the names of all channels
     const [groupedChannels, setGroupedChannels] = useState({}); // Hold the info of channels grouped by their names
-    const [uniqueChannelNames, setUniqueChannelNames] = useState('');
+    const [uniqueChannelNames, setUniqueChannelNames] = useState([]);
     const [showChannelList, setShowChannelList] = useState(false);
     const [searchedChannel, setSearchedChannel] = useState('');
 
@@ -37,6 +37,34 @@ function Header({ logoutUser, removeActiveChannel }) {
         });
     }, []);
 
+    // Group all channels by their names
+    useEffect(() => {
+        const groupedChannels = {};
+        const channelNames = [];
+        allChannels.forEach(channel => {
+            const channelName = channel.channelName.toLowerCase();
+            if (groupedChannels[channelName]) {
+                groupedChannels[channelName].push(channel);
+            } else {
+                groupedChannels[channelName] = [channel]
+            }
+            channelNames.push(channelName.replace(/\w+/g, _.capitalize)); // Capitalize the channel names
+        });
+        setAllChannelNames(channelNames);
+        setGroupedChannels(groupedChannels); // Group channels by their names
+    }, [allChannels]);
+
+    // Show unique search results based on user query
+    useEffect(() => {
+        if (query) {
+            const filteredChannels = allChannelNames.filter(channelName => {
+                return channelName.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+            });
+            const uniqueChannelNames = [...new Set(filteredChannels)];
+            setUniqueChannelNames(uniqueChannelNames);
+        }
+    }, [allChannelNames, query]);
+
     // Form doesn't submit on enter
     const removeEnterKey = (e) => {
         e.preventDefault();
@@ -44,29 +72,6 @@ function Header({ logoutUser, removeActiveChannel }) {
 
     const handleChangeQuery = (event) => {
         setQuery(event.target.value);
-    }
-
-    // When search bar is clicked, get all the available channels
-    const getAllChannels = () => {
-        axios.get('http://localhost:5000/dev-talks/channels').then(res => {
-            const channels = res.data;
-            const groupedChannels = {};
-            const channelNames = [];
-            channels.forEach(channel => {
-                const channelName = channel.channelName.toLowerCase();
-                if (groupedChannels[channelName]) {
-                    groupedChannels[channelName].push(channel);
-                } else {
-                    groupedChannels[channelName] = [channel]
-                }
-                console.log(channelName);
-                channelNames.push(_.startCase(channelName));
-            });
-            setAllChannelNames(channelNames);
-            setGroupedChannels(groupedChannels);
-        }).catch(err => {
-            console.log(err.response.data);
-        });
     }
 
     // Close search results when an option is clicked
@@ -77,22 +82,12 @@ function Header({ logoutUser, removeActiveChannel }) {
     const logOut = () => {
         logoutUser();
         removeActiveChannel();
+        removeActiveProfile();
     }
-
-    useEffect(() => {
-        const filteredChannels = allChannelNames.filter(channelName => {
-            return channelName.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-        });
-        console.log(filteredChannels);
-        const uniqueChannelNames = [...new Set(filteredChannels)];
-        setUniqueChannelNames(uniqueChannelNames);
-    }, [allChannelNames, query]);
-
-    
 
     return (
         <div className="header-wrapper">
-            <Form className="search-bar" onClick={getAllChannels} onSubmit={removeEnterKey}>
+            <Form className="search-bar" onSubmit={removeEnterKey}>
                 <FormRow
                     id="query"
                     type="search"
@@ -107,11 +102,22 @@ function Header({ logoutUser, removeActiveChannel }) {
                     ? null
                     : <div className="search-results-wrapper" onClick={closeSearchResults}>
                         {uniqueChannelNames.length === 0 ? <div className='search-result-wrapper'>(No channels found)</div> : null}
-                        {uniqueChannelNames.map((channelName, index) => 
-                            <SearchResult key={index} channelName={channelName} setShowChannelList={setShowChannelList} setSearchedChannel={setSearchedChannel} />)}
+                        {uniqueChannelNames.map((channelName, index) =>
+                            <SearchResult
+                                key={index}
+                                channelName={channelName}
+                                setShowChannelList={setShowChannelList}
+                                setSearchedChannel={setSearchedChannel}
+                            />)}
                     </div>
             }
-            <ChannelList showChannelList={showChannelList} setShowChannelList={setShowChannelList} groupedChannels={groupedChannels} searchedChannel={searchedChannel} />
+            <ChannelList
+                showChannelList={showChannelList}
+                setShowChannelList={setShowChannelList}
+                groupedChannels={groupedChannels}
+                searchedChannel={searchedChannel}
+                uniqueChannelNames={uniqueChannelNames}
+            />
             <OverlayTrigger placement='left' overlay={<Tooltip>Logout</Tooltip>}>
                 <button className='logout-btn' onClick={logOut}><ExitToAppIcon fontSize='large' /></button>
             </OverlayTrigger>
@@ -122,7 +128,8 @@ function Header({ logoutUser, removeActiveChannel }) {
 const mapDispatchToProps = dispatch => {
     return {
         logoutUser: () => dispatch(logoutUser()),
-        removeActiveChannel: () => dispatch(removeActiveChannel())
+        removeActiveChannel: () => dispatch(removeActiveChannel()),
+        removeActiveProfile: () => dispatch(removeActiveProfile())
     }
 }
 
